fac48394415f5889a4e3808e4bb723c8f3b77239
ZI Suzon <suzon.zahid@gmail.com>
2017-05-06 18:13:51 +0600

    Whole App

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h1>Trade or View your portfolio</h1>
+		<h6>You may save and load your data</h6>
+		<h6>Click on 'End Day' to begin new day</h6>
+		<hr>
+		<p>Your Funds: {{ funds }}</p>
+	</div>
+</template>
+<script>
+	export default {
+		computed: {
+			funds() {
+				return this.$store.getters.funds
+			}
+		}
+	}
+</script>
